1dbb544a5b8cc3e4eae9325aa77f3812ab306200
Jonathan Laurent <jlaurent4@student.gsu.edu>
2022-03-19 15:47:14 -0400

    added ability to delete pic and txt files after adding to excel sheet

--- parser_money_transfers.py
+++ parser_money_transfers.py
@@ -4,6 +4,7 @@ import re
 import csv
 import base_parser
 from OCR import colors_dict
+# from color_dict import colors_dict
 
 #If cloning from GitHub, make sure you change the file paths for the text files
 
@@ -60,33 +61,46 @@ class MoneyTransfers:
         add_amounts = []
         content_lower = content.lower()
         count = content.count("Add Cash")
-        if count>1:
+        # some picturs have multiple cash amounts that need to be added up, so this statement checks for that
+        if count>2:
+            if 'amount' in content_lower:
+                the_amount = self.find_money_values(content, result)
+                for j, number in enumerate(the_amount):
+                    the_amount[j] = self.clean_number(number)
+                return [float(max(the_amount))]
             add_amounts = self.find_money_values(content, add_amounts)
             for i, value in enumerate(add_amounts):
                 add_amounts[i] = int(value)
             return [sum(add_amounts)]
+        # cash amounts after 'watchlist' are invalid
         if 'watchlist' in content_lower:
             end = content_lower.find('watchlist')
             new_content1 = content_lower[:end]
             result = self.find_money_values(new_content1, result)
+        # cash amounts before 'total' are invalid
         elif 'total' in content_lower:
             start = content_lower.find('total')
             new_content = content_lower[start:]
             result = self.find_money_values(new_content, result)
+        # picture specific check (not encouraged)
         elif 'bortow' in content_lower:
             end = content_lower.find('bortow')
             new_content = content_lower[:end]
             result = self.find_money_values(new_content, result)
+        # picture specific check (not encouraged)
         elif 'paypal' in content_lower and 'limits' in content_lower:
             return [" "]
         else:
             result = self.find_money_values(content, result)
+        # created a set to hold all the valid cash values that were found in the picture
         final = set()
         if len(result) == 1:
+            # if this check is true, the only value shown is bitcoin
             if 'btc' in content.lower():
                 list_result = list(result)
                 list_result[0] = list_result[0] + " BTC"
                 return list_result
+        # if there are more than one valid values, the correct value is the max value.
         if len(result) > 1:
             for i in result:
                 num = self.clean_number(i)
@@ -99,22 +113,23 @@ class MoneyTransfers:
             return result
 
     def digital_pay_type(self, content):
-        """check names before checking most dominant colors
-        ie if it says paypal there is no need to check if there is blue.
-        for cashapp, check if there is a username ($CashappName) before
-        checking for the green color"""
+        """identifies digital payment type"""
+        # checks names before checking most dominant colors
+        # For example if the picture says paypal there is no need to check if there is blue.
         dig_type_list = {"paypal", "cash", "coinbase", "portfolio", "asset", "btc", 
             "message", "bnb", "želle", "mobile check", "gift card", "zelle"}
         res = []
         content_lower = content.lower()
         pid = int(transfer.photo_id)
         c_d = colors_dict[pid]
+        # picture that displays limits for potential payments is coinbase
         if 'limits' in content_lower and 'paypal' in content_lower:
             return['coinbase']
         for i in dig_type_list:
             if i in content_lower:
                 res.append(i)
         if res:
+            # if a picture has bitcoin and is blue dominant it is coinbase
             if 'btc' in res:
                 j = 0
                 while j < len(c_d):
@@ -123,21 +138,24 @@ class MoneyTransfers:
                         return res
                     j += 4
         if not res:
-            # add regular expression check here for cashapp usernames
+            # checks for cashapp usernames
             match = re.search(r"\$(?=.*[a-zA-Z])[a-zA-Z\d]{1,20}", content)
             match2 = re.search(r"S(?=.*[a-zA-Z])[a-zA-Z\d]{1,20}", content)
+            # checks for emails
             email = re.search(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b", content)
             username = ""
+            # if cashapp username is found, type is cashapp
             if match:
                 username = match.group(0)[1:]
                 if len(username) > 0 and not username.isnumeric():
                     res.append('Cashapp')
                     return res
                 elif match2 and 'today' in content_lower:
-                    username = match2.group(0)[1:] #wont work because it also captures 'Success'
+                    username = match2.group(0)[1:] 
                     if len(username) > 0 and not username.isnumeric():
                         res.append('Cashapp')
                         return res
+            # if an email is found, check if the dominant color is blue. If it is, it's paypal
             if email:
                 i = 0
                 r, g, b, = None, None, None
@@ -149,13 +167,11 @@ class MoneyTransfers:
                     if c_d[i+1] > c_d[i+3] and c_d[i+1] > c_d[i+2]:
                         r = True
                     i += 4
-                # print(r, " ", g, " ", b)
                 if g and b:
-                    # print("this is a b of a")
                     res.append('paypal')
                     return res
+            # if keywords are not found, check for dominant colors.
             if not res:
-                # print(c_d)
                 i = 0
                 r, g, b, = None, None, None
                 while i < len(c_d):
@@ -166,19 +182,20 @@ class MoneyTransfers:
                     if c_d[i+1] > c_d[i+3] and c_d[i+1] > c_d[i+2]:
                         r = True
                     i += 4
-                # print(r, " ", g, " ", b)
+                # green and blue dominant with 'success' and 'done' is b of a
                 if g and b and 'success' in content_lower and 'done' in content_lower:
-                    # print("this is a b of a")
                     res.append('Bank of America App')
                     return res
                 j = 0
+                # green dominant is cashapp
                 while j < len(c_d):
                     if c_d[j+2] > c_d[j+1] and c_d[j+2] > c_d[j+3]:
                         res.append('CashApp')
                         break
                     j += 4
                 if not res:
-                    i = 0    
+                    i = 0   
+                    # blue dominant is paypal
                     while i < len(c_d):
                         if c_d[i+3] > c_d[i+1] and c_d[i+3] > c_d[i+2]:
                             res.append('Paypal')
@@ -188,77 +205,93 @@ class MoneyTransfers:
 
     def data_assign_rowby(self, photo_id, date, mt_application, dig_payment, dig_payment_type, amount, official_dep, writer):
         """assigns the values to the excel sheets"""
-        #num_amount = len(amount)
-        # amount = [float(i) for i in amount]
-        # amount.sort()
         writer.writerow([photo_id, date, mt_application, dig_payment, dig_payment_type, amount, official_dep])
 
 
 if __name__ == '__main__':
     # Update this path with the path to the text files on your system
-    folder_textdoc_path = 'C:/Users/jonat/OneDrive/Documents/EBCSphotosText/Textfiles'
+    # REMOVE FILE AFTER USING IT
+    folder_textdoc_path = 'C:/Users/jonat/OneDrive/Documents/EBCS Research/MoneyTransferApps-Textfiles'
+    # folder_textdoc_path = 'C:/Users/jonat/OneDrive/Documents/EBCS Research/TestText'
     textdoc_paths = base_parser.get_textdoc_paths(folder_textdoc_path)
     headerList = ['Pic ID', 'Date', 'Money Transfer Application', 'Digital Payments', 
         'Digital Payment Type', 'Amount', 'Official Deposit']
     with open('money_transfers1_file'+'.csv','w', newline='', encoding='utf-8') as f1:
         dw = csv.DictWriter(f1, delimiter=',', fieldnames=headerList)
         dw.writeheader()
-        writer=csv.writer(f1, delimiter=',')#lineterminator='\n',
-        for text_doc in textdoc_paths:
-            writer=csv.writer(f1, delimiter=',')
-            transfer = MoneyTransfers()
-            file_name = os.path.basename(text_doc)
+        writer=csv.writer(f1, delimiter=',')# lineterminator='\n',
+        try:
+            for text_doc in textdoc_paths:
+                writer=csv.writer(f1, delimiter=',')
+                transfer = MoneyTransfers()
+                file_name = os.path.basename(text_doc)
 
-            #### parse photo id and date
-            photo_id, date = transfer.get_date_and_id_from_title(file_name)
-            if photo_id:
-                transfer.photo_id = photo_id
-            if date:
-                transfer.date = date
-            print(photo_id)
-            with open(text_doc, encoding = "utf-8") as f:
-                content = f.readlines()
-            if content:
-                text_des = content[-1]
-            else:
-                writer.writerow([transfer.photo_id, transfer.date, "", "", "", ""])
-                continue
-            digital_pay = transfer.digital_pay_type(text_des)
-            if digital_pay:
-                if digital_pay[0] == "cash":
-                    transfer.dig_payment_type = "CashApp"
-                elif digital_pay[0] == "portfolio" or digital_pay[0] == "asset":
-                    transfer.dig_payment_type = "Coinbase"
-                elif digital_pay[0] == "bnb":
-                    transfer.dig_payment_type = "Binance"
-                elif digital_pay[0] == "message":
-                    transfer.dig_payment_type = "Facebook Shop"
-                elif digital_pay[0] == "želle" or digital_pay[0] == "zelle":
-                    transfer.dig_payment_type = "Zelle"
+                #### parse photo id and date
+                photo_id, date = transfer.get_date_and_id_from_title(file_name)
+                if photo_id:
+                    transfer.photo_id = photo_id
+                if date:
+                    transfer.date = date
+                print(photo_id)
+                with open(text_doc, encoding = "utf-8") as f:
+                    content = f.readlines()
+                if content:
+                    text_des = content[-1]
                 else:
-                    transfer.dig_payment_type = digital_pay[0]
-            transfer.dig_payment = '1'
-            transfer.mt_application = '1'
-
-            #parse zipcode and state
-            if content:
-                info_amount = transfer.transfer_amount(text_des)
-                if info_amount[0] == " ":
-                    transfer.amount = 0
+                    writer.writerow([transfer.photo_id, transfer.date, "", "", "", ""])
+                    continue
+                # find digital pay type
+                digital_pay = transfer.digital_pay_type(text_des)
+                if not digital_pay:
+                    transfer.dig_payment_type = "No type identified"
                 else:
-                    if isinstance(info_amount[0], str):
-                        if 'BTC' not in info_amount[0]:
-                            final = " "
-                            for i, value in enumerate(info_amount[0]):
-                                if value == ",":
-                                    continue
-                                else:
-                                    final += value
-                            transfer.amount = float(final)
-                        else:
-                            transfer.amount = info_amount[0]
+                    if digital_pay[0] == "cash":
+                        transfer.dig_payment_type = "CashApp"
+                    elif digital_pay[0] == "portfolio" or digital_pay[0] == "asset":
+                        transfer.dig_payment_type = "Coinbase"
+                    elif digital_pay[0] == "bnb":
+                        transfer.dig_payment_type = "Binance"
+                    elif digital_pay[0] == "message":
+                        transfer.dig_payment_type = "Facebook Shop"
+                    elif digital_pay[0] == "želle" or digital_pay[0] == "zelle":
+                        transfer.dig_payment_type = "Zelle"
                     else:
-                        transfer.amount = float(info_amount[0])
-            transfer.data_assign_rowby(transfer.photo_id, transfer.date, transfer.mt_application,
+                        transfer.dig_payment_type = digital_pay[0]
+                transfer.dig_payment = '1'
+                transfer.mt_application = '1'
+
+                # find transfer amount
+                if content:
+                    info_amount = transfer.transfer_amount(text_des)
+                    if not info_amount:
+                        transfer.amount = 0
+                    else:
+                        if info_amount[0] == " ":
+                            transfer.amount = 0
+                        else:
+                            # if the amount is a string then it is Bitcoin
+                            if isinstance(info_amount[0], str):
+                                if 'BTC' not in info_amount[0]:
+                                    final = " "
+                                    for i, value in enumerate(info_amount[0]):
+                                        if value == ",":
+                                            continue
+                                        else:
+                                            final += value
+                                    transfer.amount = float(final)
+                                else:
+                                    transfer.amount = info_amount[0]
+                            else:
+                                transfer.amount = float(info_amount[0])
+                transfer.data_assign_rowby(transfer.photo_id, transfer.date, transfer.mt_application,
                 transfer.dig_payment, transfer.dig_payment_type,
                 transfer.amount, transfer.official_dep, writer)
+                del colors_dict[int(photo_id)]
+                del_file = 'C:/Users/jonat/OneDrive/Documents/EBCS Research/MoneyTransferApps-Textfiles/' + file_name
+                os.remove(del_file)
+                picture_file = file_name[0:-3] + "jpg"
+                the_picture_file = "C:/Users/jonat/OneDrive/Documents/EBCS Research/MoneyTransferApps-Photos/" + picture_file
+                os.remove(the_picture_file)
+        except Exception as e:
+            print(e)
+            print(colors_dict)
